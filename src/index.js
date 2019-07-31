5cbc1bac5b4fda338d7023b914bb8ec14aa15636
ThatFellowAintMellow <danielgreetham@gmail.com>
2019-07-31 20:50:16 +0100

    identified hex value

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ request('https://api.noopschallenge.com/hexbot', function (error, response, body
   console.log('error:', error)
   console.log('statusCode:', response && response.statusCode)
   console.log('body:', body)
+  var response = JSON.parse(body) 
+  var hex = response.colors[0].value
+  console.log(hex)
 })
