44703e87c277378eff64d6af7d463290272ebe0f
Jonathan Cowling <09cowlingj@gmail.com>
2019-07-31 17:59:05 +0100

    Add request library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+var request = require('request')
+
+request('http://www.example.com', function (error, response, body) {
+  console.log('error:', error)
+  console.log('statusCode:', response && response.statusCode)
+  console.log('body:', body)
+})
