dd75094b8ed2d3dee5f27bbab1e9b3abd9a68da9
Jonathan Cowling <09cowlingj@gmail.com>
2019-07-31 20:26:22 +0100

    Add ask the user a question

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 var request = require('request')
+var readline = require('readline')
 
-request('http://www.example.com', function (error, response, body) {
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+request('https://api.noopschallenge.com/hexbot', function (error, response, body) {
   console.log('error:', error)
   console.log('statusCode:', response && response.statusCode)
   console.log('body:', body)
+
+  reader.question('identify the following color: \'#XXXXXX\'', function (answer) {
+    console.log(answer)
+  })
 })
